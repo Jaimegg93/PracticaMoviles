531f728cfe79f798ffd8867ddd55eecb02d94d7a
jaime <jaimegg93@educastur.es>
2025-03-09 22:01:43 +0100

    Pracitica acabada

---
+++ app/src/main/java/com/example/garciajaimeparcial2/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.garciajaimeparcial2
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val calculadoraImageView: ImageView = findViewById(R.id.calculadoraImageView)
+        val botonImageView: Button = findViewById(R.id.loginImageView)
+
+        calculadoraImageView.setOnClickListener {
+            val intent = Intent(this, calculadora::class.java)
+            startActivity(intent)
+        }
+        botonImageView.setOnClickListener {
+            val intent = Intent(this, Log_In::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
